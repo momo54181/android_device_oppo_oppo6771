cba479bb0072e3de3d2a46f7996ce30fdf3a7e39
Momo5418 <momo5418@mocn.top>
2024-10-05 11:54:15 +0800

    更新 omni_PACM00.mk

--- omni_PACM00.mk
+++ omni_PACM00.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from PACM00 device
 $(call inherit-product, device/oppo/PACM00/device.mk)
